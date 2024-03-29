16e52a3f86cbcf9846600a7756e8dc7e4edf6b68
alin.fanu <student@b528a-6.intranet.cs.upt.ro>
2024-03-29 17:39:14 +0200

    incercare lab SO

--- SO_Proiect.c
+++ SO_Proiect.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-int main()
+int main(int argc , char *argv[])
 {
-    printf("ceva\n");
+    DIR *dir = opendir(argv[1]);
+    struct stat st;
+    struct dirent *d;
+
+    if(argc != 2){
+        fprintf(stderr , "numar incorect de variabile\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(opendir(argv[1]) == NULL){
+        fprintf(stderr , "Eroare la deschiderea directorului\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(stat(argv[1] , &st) < 0){
+        fprintf(stderr , "eroare\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while((d = readdir(dir)) != NULL){
+        printf("%s\n" , d->d_name);
+
+    }
+
+    
+
     return 0;
 }
